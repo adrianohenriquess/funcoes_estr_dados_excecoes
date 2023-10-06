59f6d8a4565f1072fd4978b87c0af381f741e8e2
Adriano Henrique de Souza da Silva <adriano.henriquesds@gmail.com>
2023-10-05 22:21:41 -0300

    Criação da classe conta com métodos coesos

--- introducao/oo/conta.py
+++ introducao/oo/conta.py
@@ -1,8 +1,20 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("construindo objeto ... {}".format(self))
-        self.numero  = numero
-        self.titular = titular
-        self.saldo   = saldo
-        self.limite  = limite
+        self.__numero  = numero
+        self.__titular = titular
+        self.__saldo   = saldo
+        self.__limite  = limite
     
+    def extrato(self):
+        print("Saldo de {} do titular {} ".format(self.__saldo, self.__titular))
+
+    def deposita(self, valor):
+        self.__saldo += valor
+
+    def saca(self, valor):
+        self.__saldo -= valor
+
+    def transfere(self, valor, conta_destino):
+        self.saca(valor)
+        conta_destino.deposita(valor)
