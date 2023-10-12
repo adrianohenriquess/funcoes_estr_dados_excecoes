4f934e96727566baa0a9defe2a7435744cd470ed
adrianohenriquess <adriano.henriquesds@gmail.com>
2023-10-11 23:07:10 -0300

    Simplificando a exibição dos dados de filme e serie

--- introducao/oo2/modelo.py
+++ introducao/oo2/modelo.py
@@ -31,13 +31,9 @@ class Serie(Programa):
 
 vingadores = Filme("Vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
-print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano} '
-      f'- Duração: {vingadores.duracao}'
-      f'- Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano} '
-      f'- Temporadas: {atlanta.temporadas} '
-      f'- Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
