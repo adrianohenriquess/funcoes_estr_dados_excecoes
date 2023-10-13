7cb456f078ea405edd7752356d5d3a2a64fadd58
adrianohenriquess <adriano.henriquesds@gmail.com>
2023-10-12 21:48:22 -0300

    Usando representação de texto para um objeto com __str__, muito parecido com o toString do java

--- introducao/oo2/modelo.py
+++ introducao/oo2/modelo.py
@@ -19,21 +19,31 @@ class Programa:
     def dar_like(self):
         self._likes += 1
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
+
 vingadores = Filme("Vingadores - guerra infinita", 2018, 160)
 vingadores.dar_like()
-print(f'{vingadores.nome} - {vingadores.duracao}: {vingadores.likes}')
-
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(f'{atlanta.nome} - {atlanta.temporadas}: {atlanta.likes}')
+
+filmes_e_series = [vingadores, atlanta]
+for programa in filmes_e_series:
+    print(programa)
